a150a13a76b158b03b781514736970e879260431
Acestus <architect@acestus.com>
2022-05-02 13:01:48 -0500

    API test

--- Topics/Invoking a function/Young and beautiful/main.py
+++ Topics/Invoking a function/Young and beautiful/main.py
@@ -2,5 +2,5 @@ jack_age = int(input())
 alex_age = int(input())
 lana_age = int(input())
 
-young_age = min(jack_age, alex_age, lana_age)
-print(young_age)
+youngest_age = min(jack_age, alex_age, lana_age)
+print(youngest_age)
