91a5852cb51131ce887034380aa43d5d7b71d35d
Acestus <architect@acestus.com>
2022-05-08 14:57:30 -0500

    creating lists

---
+++ Topics/Comparisons/Order/main.py
@@ -0,0 +1,4 @@
+first_number = input()
+second_number = input()
+third_number = input()
+print(first_number < second_number < third_number)
---
+++ Topics/Comparisons/Very odd/main.py
@@ -0,0 +1,5 @@
+dividend = int(input())
+divisor = int(input())
+division = dividend // divisor
+remainder = division % 2
+print(remainder != 0)
